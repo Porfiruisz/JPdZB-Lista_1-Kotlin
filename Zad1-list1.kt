4040ee7059acb6288958abd6437d97207b36c95d
Porfiruisz <263593@student.pwr.edu.pl>
2025-04-04 22:12:09 +0200

    Add files via upload

---
+++ Zad1-list1.kt
@@ -0,0 +1,38 @@
+package com.example.probio_lista1
+
+import kotlin.math.sqrt
+
+/**
+ * @author Stanisław Wojtków
+ * Oblicza pole trójkąta przy użyciu wzoru Herona.
+ *
+ * @param a długość pierwszego boku (musi być dodatnia)
+ * @param b długość drugiego boku (musi być dodatnia)
+ * @param c długość trzeciego boku (musi być dodatnia)
+ * @return pole powierzchni trójkąta jako liczba zmiennoprzecinkowa typu Double
+ *
+ * @throws IllegalArgumentException jeśli którykolwiek z boków jest niedodatni
+ * @throws IllegalArgumentException jeśli suma dwóch boków nie jest większa niż trzeci bok (nie tworzy trójkąta)
+ */
+
+
+fun heron(a: Double, b: Double, c: Double): Double{
+    require(a > 0 && b > 0 && c > 0) { "Dlugosci bokow musza byc dodatnie" } // wyjątek jeśli wprowadzone argumenty są mniejsze bądź równe zero
+    require(a + b > c && a + c > b && b + c > a) { "Podane wartosci nie tworza trojkata" } // wyjątek jeśli suma dwóch boków jest mniejsza od trzeciego
+
+    val q = (a+b+c)/2
+    val P: Double = sqrt(q*(q-a)*(q-b)*(q-b))
+
+    return P
+}
+
+fun main(){
+    val testHeron1 = heron(3.0,4.0,5.0) //otrzymujemy poprawną wartość
+    println(testHeron1)
+
+    val testHeron2 = heron(-3.0,4.0,5.0) //otrzymujemy wyjątek "Dlugosci bokow musza byc dodatnie"
+    println(testHeron2)
+
+    val testHeron = heron(30.0,4.0,5.0) //otrzymujemy wyjątek "Podane wartosci nie tworza trojkata"
+    println(testHeron)
+}
---
+++ Zad3-list1.kt
@@ -0,0 +1,59 @@
+package com.example.probio_lista1
+import kotlin.math.pow
+/**
+ * Funkcja zwraca listę podzbiorów zbioru wejściowego
+ *
+ * @author Stanisław Wojtków
+ *
+ * @param zbior zbiór elementów dowolnego typu
+ * @return lista wszystkich podzbiorów zbioru wejściowego
+ *
+ * @throws IllegalArgumentException jeśli zbiór jest pusty
+ * @throws IllegalArgumentException jeśli zbiór zawiera nieprawidłowy element
+ */
+
+
+fun <T> podzbiory(zbior: Set<T>): List<Set<T>> {
+    require(zbior.isNotEmpty()) { "Zbior nie moze byc pusty" }
+
+    if (zbior.any { it is String && it.isEmpty() }) {
+        throw IllegalArgumentException("Zbior zawiera nieprawidlowy element") // Wyjątek, gdy funkcja zawiera pusty element ciągu (np. " ")
+    }
+
+    val lista = zbior.toList()
+    val n = lista.size
+    val wynik = mutableListOf<Set<T>>()
+    val dwa = 2.0
+
+    for (i in 1 until dwa.pow(n).toInt()) {  // dwa.pow(n).toInt(), czyli wszystkie kombinacje 2^n
+        val podzbior = mutableSetOf<T>()
+        for (j in 0 until n) {                 // ta część zrobiona z pomocą czatu GPT
+            if (i and (dwa.pow(j).toInt()) != 0) {
+                podzbior.add(lista[j])
+            }
+        }
+        wynik.add(podzbior)
+    }
+
+    return wynik
+}
+
+
+
+fun main(){
+
+    //Funkcja działa nieco "chaotycznie", ale nie mogłem wymyśleć niczego lepszego
+
+    val test1 = setOf('A','B','C') //Poprawne wywołanie funkcji
+    println(podzbiory(test1))
+
+    val test2 = setOf("") // Funkcja zwraca wyjątek
+    println(podzbiory(test2))
+
+    val test3 = setOf('1','2',3) //Poprawne wywołanie funkcji (nawet nie mając jednego z argumentów w apostrofach)
+    println(podzbiory(test3))
+
+    val test4 = setOf('1','A',3) //Poprawne wywołanie funkcji
+    println(podzbiory(test4))
+
+}
